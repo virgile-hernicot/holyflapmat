cbc731f48a7f4f627808335624987586027f6c66
Virgile Hernicot <virgile.hernicot@epfl.ch>
2018-11-25 01:39:14 +0100

    colored map

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 make
 echo "compilation done.\n"
 #python3 manyusers.py
->>>>>>> 4dc17a09ec240aeabf4221bc2e460a93ba46fe01
 echo "Location script done.\n"
 ./cff_flow < ./costs_data > result.txt
 echo "Flow done.\n"
